26e1c5d39502e1405f20d3424ef3660a02d302c6
BobHell0 <anujan.cenan@gmail.com>
2026-05-10 09:52:07 +1000

    prototype for the get function plus adjusted add function to include the void* value

--- zobrist_hashing/include/zobrist_hashing.h
+++ zobrist_hashing/include/zobrist_hashing.h
@@ -1,3 +1,6 @@
+#ifndef ZOBRIST_HASHING_H
+#define ZOBRIST_HASHING_H
+
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
@@ -10,14 +13,16 @@
 
 
 typedef Hash_Set Set;
-
-
 typedef struct Zobrist_Element Zobrist_Element;
 typedef struct Zobrist Zobrist;
 
 
 Zobrist* zobrist_init(int upper_bound);
-void zobrist_add(Zobrist* zobrist, Set* s);
+void zobrist_add(Zobrist* zobrist, Set* s, void* value);
 bool zobrist_exists(Zobrist* zobrist, Set* s);
 void zobrist_free(Zobrist* z);
 int get_num_elements(Zobrist* z);
+
+void* zobrist_get(Zobrist* zobrist, Set* s);
+
+#endif
